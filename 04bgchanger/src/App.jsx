7d2e5cc35e5c348949371610810f92fad7767c4f
ayushraisim <ayushraisim@gmail.com>
2026-02-01 16:18:33 +0530

    have made bgchanger with buttons learnt onclick and refresh default setting

---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,41 @@
+import {useState} from "react"
+
+function App() {
+  const [color,setColor] = useState("olive")
+
+  return (
+    <>
+     <div className="w-full h-screen duration-200"
+     style = {{backgroundColor : color}}
+     ></div>
+     <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+      <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+        <button
+        onClick={()=>{setColor("red")}} // onclick expects a function  but setcolor can be passed directly but its a reference ,,, we are using a callback function because expects a function and in callback we can pass parameters ,, so if refernce is passed then function is returning someting and it will be given to onclick and we have to pass parameters so we have used callback function
+        className="outline-none px-4 py-1 rounded-full shadow-lg"
+        style={{backgroundColor: "red"}}
+        >
+          Red
+        </button>
+         <button
+         onClick={()=>{setColor("green")}}
+        className="outline-none px-4 py-1 rounded-full shadow-lg"
+        style={{backgroundColor: "green"}}
+        >
+          Green
+        </button>
+         <button
+         onClick={()=>{setColor("pink")}}
+        className="outline-none px-4 py-1 rounded-full shadow-lg"
+        style={{backgroundColor: "pink"}}
+        >
+          Pink
+        </button>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
+//when we referesh pag all states are reset and it is set tob olive by default 
